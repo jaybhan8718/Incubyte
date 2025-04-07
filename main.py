a856dc4b630dc53cebe4ae87c2564b452b84677e
Jaibhan.Gaur <Jaibhan.Gaur@LT0198525>
2025-04-07 16:59:58 +0530

    Handling new lines between numbers

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ def add(numbers:str):
     if numbers == "": #handling empty string
         return sum
     else:
-        numbers = numbers.split(",") #handling values if passed comma seperated
+        #handle new lines between numbers and also comma seperated value
+        if "\n" in numbers:
+            numbers = numbers.split("\n")
+        else:
+            numbers = numbers.split(",")
         for num in numbers:
-            sum += int(num)
+            if isinstance(num, str):
+                for n in num.split(","):
+                    sum += int(n)
+            else:
+                print(num)
+                sum += int(num)
     return sum
 
 assert add("") == 0, "Test case failed: Expected 0 for empty string"
 assert add("1,2,3") == 6, "Test case failed: Expected 6 for '1,2,3'"
-assert add("4,5,6") == 12, "Test case failed: Expected 15 for '4,5,6'"
+assert add("1\n2,3") == 6, "Test case failed: Expected 6 for '1\\n2,3'"
+assert add("4\n5\n6") == 15, "Test case failed: Expected 15 for '4\\n5\\n6'"
