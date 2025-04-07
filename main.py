13216f2be3f0690206755f685681b270833c61a0
Jaibhan.Gaur <Jaibhan.Gaur@LT0198525>
2025-04-07 16:48:22 +0530

    Handling multiple numbers passed as comma seperated values

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def add(numbers:str):
+    sum = 0
     if numbers == "": #handling empty string
-        return 0
-    
-    
+        return sum
+    else:
+        numbers = numbers.split(",") #handling values if passed comma seperated
+        for num in numbers:
+            sum += int(num)
+    return sum
+
+assert add("") == 0, "Test case failed: Expected 0 for empty string"
+assert add("1,2,3") == 6, "Test case failed: Expected 6 for '1,2,3'"
+assert add("4,5,6") == 12, "Test case failed: Expected 15 for '4,5,6'"
